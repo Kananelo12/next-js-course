e8ed6307d44385f38443d57cd0f48cfe3bab2dfe
Kananelo12 <kananeloj12@gmail.com>
2025-05-26 13:54:54 +0200

    Designed the Startup Card UI with Mock data, and installed sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
-export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>}) {
-  const query = (await searchParams).query
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Joel" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://joel-portfolio.web.app/images/about-img.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -15,6 +32,22 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
